95dd7a02b2b5465af4436cd10a4be68961ee4d4e
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2025-07-03 10:27:13 +0200

    ex 2.9 done, add filter ability to the phonebook app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 
-const Person = ({person}) => {
-  return (
-    <p>{person.name} {person.phone}</p>
-  )
+const Person = ({person, filter}) => {
+  if(filter !== "") {
+    const startPosition = person.name.toLowerCase().indexOf(filter.toLowerCase())
+    const before = person.name.slice(0, startPosition)
+    const match = person.name.slice(startPosition, startPosition + filter.length)
+    const after = person.name.slice(startPosition + filter.length, person.name.lenght)
+
+    return <p>{before}<strong>{match}</strong>{after} {person.number}</p>
+
+  } else {
+    return (
+      <p>{person.name} {person.number}</p>
+    )
+  }
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [filter, setNewFilter] = useState('')
 
   const handleNameInputChange = (event) => {
     setNewName(event.target.value)
@@ -17,6 +33,10 @@ const App = () => {
 
   const handlePhoneNumberInputChange = (event) => {
     setNewPhoneNumber(event.target.value)
+  }
+
+  const handleFilterInputChange = (event) => {
+    setNewFilter(event.target.value)
   }
 
   const handleFormSubmit = (event) => {
@@ -28,7 +48,8 @@ const App = () => {
       } else {
         const newPerson = {
           name: newName,
-          phone: newPhoneNumber
+          number: newPhoneNumber,
+          id: String(persons.length+1)
         }
         setPersons(persons.concat(newPerson))
         setNewName("")
@@ -37,9 +58,16 @@ const App = () => {
     }
   }
 
+  const filteredPerson = (filter !== "")
+  ? persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+  : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterInputChange}></input>
+      </div>
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameInputChange}/>
@@ -52,8 +80,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <Person key={person.name} person={person} />
+      {filteredPerson.map(person => 
+        <Person key={person.id} person={person} filter={filter} />
       )}
     </div>
   )
