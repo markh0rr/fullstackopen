4ecbda67644f03ad50cae1c42961e437d549a0ab
Benoît MORAWIEC <78908194+B-mellon@users.noreply.github.com>
2025-07-03 09:48:32 +0200

    2.6 phonebook step1 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,49 @@
-function App() {
-  return (
-    <>
+import { useState } from 'react'
 
-    </>
+const Person = ({person}) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameInputChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+    if(newName !== "") {
+      const newPerson = {
+        name: newName
+      }
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNameInputChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <p>debug: {newName}</p>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <Person key={person.name} person={person} />
+      )}
+    </div>
   )
 }
 
